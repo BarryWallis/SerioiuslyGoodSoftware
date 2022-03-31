000a3e90a9f7feb27f00587f49425a3a2f3966c0
Barry Wallis <barry.wallis@outlook.com>
2022-03-31 16:15:58 -0700

    Chapter 1

---
+++ Novice/Container.cs
@@ -0,0 +1,78 @@
+﻿namespace Novice;
+public class Container
+{
+    /// <summary>
+    /// The group of connected containers.
+    /// </summary>
+    private readonly Container[] _g;
+
+    /// <summary>
+    /// The actual size of the group.
+    /// </summary>
+    private int _n;
+
+    /// <summary>
+    /// The water amount in this container.
+    /// </summary>
+    public double Amount { get; private set; }
+
+    /// <summary>
+    /// Initializes a new empty <see cref="Container"/>.
+    /// </summary>
+    public Container()
+    {
+        // Look: a magic number
+        _g = new Container[1_000];
+
+        // Put this container in its group
+        _g[0] = this;
+        _n = 1;
+        Amount = 0;
+    }
+
+    /// <summary>
+    /// Add water to the container.
+    /// </summary>
+    /// <param name="x">The amount of water to add.</param>
+    public void AddWater(double x)
+    {
+        double y = x / _n;
+        for (int i = 0; i < _n; i++)
+        {
+            _g[i].Amount = _g[i].Amount + y;
+        }
+    }
+
+    /// <summary>
+    /// Connect this <see cref="Container"/> to the given <see cref="Container"/>.
+    /// </summary>
+    /// <param name="c">The <see cref="Container"/> to connect to this one.</param>
+    public void ConnectTo(Container c)
+    {
+        // Amount per container
+        double z = ((Amount * _n) + (c.Amount * c._n)) / (_n + c._n);
+
+        // For each container _g[i] in first group
+        for (int i = 0; i < _n; i++)
+        {
+            // For each container _g[j] in the second group
+            for (int j = 0; j < c._n; j++)
+            {
+                // Append c._g[j] to group _g[i]
+                _g[i]._g[_n + j] = c._g[j];
+
+                // Append _g[i] to group c._g[j]
+                c._g[j]._g[c._n + i] = _g[i];
+            }
+        }
+
+        _n += c._n;
+
+        // Update sizes and amounts
+        for (int i = 0; i < _n; i++)
+        {
+            _g[i]._n = _n;
+            _g[i].Amount = z;
+        }
+    }
+}
---
+++ NoviceTests/ContainerTests.cs
@@ -0,0 +1,95 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Novice;
+
+namespace NoviceTests;
+
+[TestClass]
+public class ContainerTests
+{
+    [TestMethod]
+    public void Ctor_Default_ReturnsZero()
+    {
+        Container container = new();
+
+        double actual = container.Amount;
+
+        Assert.AreEqual(0, actual);
+    }
+
+    [TestMethod]
+    public void GetAmount_DefaultContainer_ReturnsZero()
+    {
+        Container container = new();
+
+        double actual = container.Amount;
+
+        Assert.AreEqual(0, actual);
+    }
+
+    [TestMethod]
+    public void AddWater_AddWaterToOneContainer_ReturnsTheAmountAdded()
+    {
+        const double amountToAdd = 5.5;
+        Container container = new();
+
+        container.AddWater(amountToAdd);
+
+        Assert.AreEqual(amountToAdd, container.Amount);
+    }
+
+    [TestMethod]
+    public void ConnectTo_ConnectTwoContainers_ReturnsAmountSplitBetweenTheContainers()
+    {
+        const double amountToAdd = 12.0;
+        Container container1 = new();
+        Container container2 = new();
+        container1.AddWater(amountToAdd);
+
+        container1.ConnectTo(container2);
+
+        Assert.AreEqual(amountToAdd / 2, container1.Amount);
+        Assert.AreEqual(amountToAdd / 2, container2.Amount);
+    }
+
+    [TestMethod]
+    public void ConnectTo_ConnectThreeContainers_ReturnsSameAmountInAllThree()
+    {
+        const double amountToAdd = 12.0;
+        Container container1 = new();
+        Container container2 = new();
+        Container container3 = new();
+        container1.AddWater(amountToAdd);
+
+        container1.ConnectTo(container2);
+        container2.ConnectTo(container3);
+
+        Assert.AreEqual(amountToAdd / 3, container1.Amount);
+        Assert.AreEqual(amountToAdd / 3, container2.Amount);
+        Assert.AreEqual(amountToAdd / 3, container3.Amount);
+    }
+
+    [TestMethod]
+    public void ConnectTo_ConnectFourContainers_ReturnsSameAmountInAllFour()
+    {
+        const double amountToAddToContainer1 = 12.0;
+        const double amountToAddToContainer4 = 8.0;
+        const double totalAmount = amountToAddToContainer1 + amountToAddToContainer4;
+        Container container1 = new();
+        Container container2 = new();
+        Container container3 = new();
+        Container container4 = new();
+        container1.AddWater(amountToAddToContainer1);
+        container4.AddWater(amountToAddToContainer4);
+
+        container1.ConnectTo(container2);
+        container2.ConnectTo(container3);
+        container2.ConnectTo(container4);
+
+        Assert.AreEqual(totalAmount / 4, container1.Amount);
+        Assert.AreEqual(totalAmount / 4, container2.Amount);
+        Assert.AreEqual(totalAmount / 4, container3.Amount);
+        Assert.AreEqual(totalAmount / 4, container4.Amount);
+    }
+}
